88a32c71f9990ae15901df181c15af94c5c9d34f
dandy <danielsbarbosa27@gmail.com>
2022-07-17 01:50:43 -0300

    Congigurando SqliteConnetcion SqliteAdm e Migrations parte 2

---
+++ provider_todo_list_adf/lib/app/core/database/sqlite_migration_factory.dart
@@ -0,0 +1,18 @@
+import 'migrations/migration.dart';
+import 'migrations/migration_v1.dart';
+import 'migrations/migration_v2.dart';
+import 'migrations/migration_v3.dart';
+
+class SqliteMigrationFactory {
+  List<Migration> getCreateMigration() => [MigrationV1(), MigrationV2()];
+
+  List<Migration> getUpgradeMigration(int version) {
+    final migrations = <Migration>[];
+    if (version == 1) {
+      migrations.add(MigrationV2());
+    }
+    if (version == 2) {
+      migrations.add(MigrationV3());
+    }
+  }
+}
