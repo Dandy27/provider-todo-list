34edcfc19c2dec962ccacb18280f0dc62855db46
dandy <danielsbarbosa27@gmail.com>
2022-08-05 11:52:57 -0300

    Add Firebase Auth ao Projeto

--- provider_todo_list_adf/lib/app/app_widget.dart
+++ provider_todo_list_adf/lib/app/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider_todo_list_adf/app/core/ui/todo_list_ui_config.dart';
 
@@ -17,6 +18,7 @@ class _AppWidgetState extends State<AppWidget> {
   @override
   void initState() {
     super.initState();
+    FirebaseAuth auth = FirebaseAuth.instance;
     WidgetsBinding.instance.addObserver(sqliteAdmConnection);
   }
 
