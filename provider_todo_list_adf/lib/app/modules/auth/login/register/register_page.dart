90ef5e11191650dd1f59cc1316b519089a95fbcd
dandy <danielsbarbosa27@gmail.com>
2022-08-18 00:05:47 -0300

    Conectando RegisterController aos nossos serviços

--- provider_todo_list_adf/lib/app/modules/auth/login/register/register_page.dart
+++ provider_todo_list_adf/lib/app/modules/auth/login/register/register_page.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:provider_todo_list_adf/app/core/widget/todo_list_field.dart';
+import 'package:provider/provider.dart';
+import 'package:validatorless/validatorless.dart';
 
 import '../../../../core/ui/theme_extensions.dart';
+import '../../../../core/validators/validators.dart';
+import '../../../../core/widget/todo_list_field.dart';
 import '../../../../core/widget/todo_list_logo.dart';
+import 'register_controller.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailEC = TextEditingController();
+  final _passwordEC = TextEditingController();
+  final _confirmPasswordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailEC.dispose();
+    _passwordEC.dispose();
+    _confirmPasswordEC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,36 +77,65 @@ class RegisterPage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
             child: Form(
-                child: Form(
-                    child: Column(
-              children: [
-                TodoListField(label: 'E-mail'),
-                const SizedBox(height: 20),
-                TodoListField(
-                  label: 'Senha',
-                  obscureText: true,
-                ),
-                const SizedBox(height: 20),
-                TodoListField(
-                  label: 'Confirma Senha',
-                  obscureText: true,
-                ),
-                const SizedBox(height: 20),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20))),
-                    child: const Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text('Salvar'),
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TodoListField(
+                      label: 'E-mail',
+                      controller: _emailEC,
+                      validator: Validatorless.multiple([
+                        Validatorless.required('Email obrigatório'),
+                        Validatorless.email('Email inválido'),
+                      ]),
                     ),
-                  ),
-                )
-              ],
-            ))),
+                    const SizedBox(height: 20),
+                    TodoListField(
+                      label: 'Senha',
+                      controller: _passwordEC,
+                      validator: Validatorless.multiple([
+                        Validatorless.required('Senha obrigatatória'),
+                        Validatorless.min(
+                            6, 'Senha deve ter pelo ao menos 5 caracteres')
+                      ]),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 20),
+                    TodoListField(
+                      label: 'Confirma Senha',
+                      controller: _confirmPasswordEC,
+                      validator: Validatorless.multiple([
+                        Validatorless.required('Senha obrigatatória'),
+                        Validators.compare(
+                            _passwordEC, 'Senha diferente de confirma senha')
+                      ]),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 20),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          final formaValid =
+                              _formKey.currentState?.validate() ?? false;
+                          if (formaValid) {
+                            final email = _emailEC.text;
+                            final password = _passwordEC.text;
+                            context
+                                .read<RegisterController>()
+                                .registerUser(email, password);
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20))),
+                        child: const Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Text('Salvar'),
+                        ),
+                      ),
+                    )
+                  ],
+                )),
           )
         ],
       ),
